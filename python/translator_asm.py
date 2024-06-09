f2c40ce3850a6bbc0084ff5bf2ea16db16b5fa2f
DanielBabenko <danielbabenko606@yandex.ru>
2024-06-09 17:25:13 +0300

    52 - GG

--- python/translator_asm.py
+++ python/translator_asm.py
@@ -6,29 +6,22 @@ import sys
 
 from isa import Opcode, Term, write_code
 
-
 def get_meaningful_token(line):
-    """Извлекаем из строки содержательный токен (метка или инструкция), удаляем
-    комментарии и пробелы в начале/конце строки.
-    """
     return line.split(";", 1)[0].strip()
 
 
 def translate_stage_1(text):
-    """Первый проход транслятора. Преобразование текста программы в список
-    инструкций и определение адресов меток.
-
-    Особенность: транслятор ожидает, что в строке может быть либо 1 метка,
-    либо 1 инструкция. Поэтому: `col` заполняется всегда 0, так как не несёт
-    смысловой нагрузки.
-    """
-    opcodes_with_operand = [Opcode.JMP, Opcode.DEC, Opcode.INC, Opcode.PRINT_CHAR, Opcode.CALL]
+    opcodes_with_operand = [Opcode.JMP, Opcode.DEC, Opcode.INC, Opcode.PRINT_CHAR]
     opcodes_with_two_operands = [Opcode.JZ, Opcode.JNZ, Opcode.JS, Opcode.JNS, Opcode.ADD_STR, Opcode.STORE]
     opcodes_with_three_operands = [Opcode.MOV, Opcode.MOD, Opcode.MUL, Opcode.SUB, Opcode.ADD]
     opcodes_with_operands = opcodes_with_operand + opcodes_with_two_operands + opcodes_with_three_operands
 
     code = []
     labels = {}
+    strings = {}
+    data = []
+
+    last_label = None
 
     for line_num, raw_line in enumerate(text.splitlines(), 1):
         token = get_meaningful_token(raw_line)
@@ -41,25 +34,47 @@ def translate_stage_1(text):
             label = token.strip(":")
             assert label not in labels, "Redefinition of label: {}".format(label)
             labels[label] = pc
+            last_label = label
+
         elif " " in token:
             sub_tokens = token.split(" ")
-            assert len(sub_tokens) == 2, "Invalid instruction: {}".format(token)
             mnemonic, arg = sub_tokens
+            arg = arg.split(",")
             opcode = Opcode(mnemonic)
-            assert opcode == Opcode.JZ or opcode == Opcode.JMP, "Only `jz` and `jnz` instructions take an argument"
+
+            assert opcode in opcodes_with_operands, f"Instruction ({opcode}) doesn't take an argument"
+
             code.append({"index": pc, "opcode": opcode, "arg": arg, "term": Term(line_num, 0, token)})
+
+            if opcode.value == Opcode.ADD_STR:
+                assert last_label is not None, "There is no label before add_str"
+                strings[last_label] = len(data)
+
+                data.append(int(arg[0]))
+                for let in arg[1][1:-1]:
+                    data.append(ord(let))
+            else:
+                code.append({"index": pc, "opcode": opcode, "arg": arg, "term": Term(line_num, 0, token)})
         else:
             opcode = Opcode(token)
             code.append({"index": pc, "opcode": opcode, "term": Term(line_num, 0, token)})
 
-    return labels, code
+    return labels, strings, code, data
 
 
-def translate_stage_2(labels, code):
+def translate_stage_2(labels, strings, code):
 
     for instruction in code:
-        if "arg" in instruction:
+        if "arg" in instruction and instruction["opcode"].value in {
+            Opcode.JZ.value,
+            Opcode.JNZ.value,
+            Opcode.JS.value,
+            Opcode.JNS.value,
+            Opcode.JMP.value
+        }:
             label = instruction["arg"]
+            if label[0].isdigit() or label[0][0] == "r" and label[0][1].isdigit():
+                continue
             assert label in labels, "Label not defined: " + label
             instruction["arg"] = labels[label]
     return code
@@ -67,8 +82,8 @@ def translate_stage_2(labels, code):
 
 def translate(text):
 
-    labels, code = translate_stage_1(text)
-    code = translate_stage_2(labels, code)
+    labels, strings, code, data  = translate_stage_1(text)
+    code = translate_stage_2(labels, strings, code)
 
     # ruff: noqa: RET504
     return code
